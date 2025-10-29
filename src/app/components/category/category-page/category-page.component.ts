b820c17ccc93638da56d5ff6ac3d40ffd6c77e51
Jean Huaringa <franciscohuaringach@gmail.com>
2025-10-29 18:32:13 -0500

    feat: implementacion de las tablas y de las paginas

--- src/app/components/category/category-page/category-page.component.ts
+++ src/app/components/category/category-page/category-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { TableComponent } from "../../table/table.component";
 
 @Component({
   selector: 'app-category-page',
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './category-page.component.html',
   styleUrl: './category-page.component.css'
 })
 export class CategoryPageComponent {
+  columnas = ['Id', 'Categoria', 'Subcategoria', 'Estado'];
 
+  data = [
+    {id: 'N°', categoria : 'Direccion', subcategoria: 'Usuario', estado: 'Ubicacion'}
+  ];
 }
--- src/app/components/product/products-page/products-page.component.ts
+++ src/app/components/product/products-page/products-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { TableComponent } from "../../table/table.component";
 
 @Component({
   selector: 'app-products-page',
-  imports: [],
+  imports: [TableComponent],
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css'
 })
 export class ProductsPageComponent {
 
+  columnas = ['Id', 'Nombre', 'Imagen', 'Precio', 'Categoria', 'Material'];
+
+  data = [
+    {id: 'N°', nombre : 'Nombre', imagen: 'Imagen', precio: 'Precio', categoria: 'Categoria', material : 'Material' }
+  ];
 }
